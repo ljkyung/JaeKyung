550a5b4c101a442c30ececa5487287cfe7faba55
JaeKyung Lee <ljjkyung@gmail.com>
2022-06-29 12:31:01 +0900

    first Commit

---
+++ AndroidStudioProjects/FastCampus/Kotlin/01/실습.ws.kts
@@ -0,0 +1,17 @@
+// 주석 -> 사람만 보기 위해서 작성
+
+// 1. var 변수 선언하기
+var changeYes = 100
+// var chage_yes // 선언과 동시에 초기화를 시켜줘야 한다.
+println(changeYes)
+
+changeYes = 200
+println(changeYes)
+
+// 2. val 변수 선언하기
+/*
+val change_no = 100
+println(change_no)
+
+change_no = 200
+println(change_no)*/
