034fb365727e17a049112fde23a4c55ce47de521
ljkyung <ljjkyung@gmail.com>
2022-08-16 08:28:21 +0900

    Add Activity

--- AndroidStudioProjects/KotlinStudy/app/src/main/java/com/example/fastcampus/TestActivity.kt
+++ AndroidStudioProjects/KotlinStudy/app/src/main/java/com/example/fastcampus/TestActivity.kt
@@ -2,12 +2,10 @@ package com.example.fastcampus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 
-class MainActivity : AppCompatActivity() {
+class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.drawable)
-        Log.d("LifeCycle", "onCreate")
+        setContentView(R.layout.activity_test)
     }
 }
