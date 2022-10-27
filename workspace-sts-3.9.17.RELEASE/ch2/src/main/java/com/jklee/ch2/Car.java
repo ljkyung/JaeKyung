cbabb0ab80794017f172ef7d64d875d2dd40e89b
JaeKyung Lee <ljjkyung@gmail.com>
2022-10-27 11:37:11 +0900

    Add Servlet, JSP

---
+++ workspace-sts-3.9.17.RELEASE/ch2/src/main/java/com/jklee/ch2/Car.java
@@ -0,0 +1,6 @@
+package com.jklee.ch2;
+
+public class Car    { 
+	private String color = "red"; 
+	public String getColor() { return color; }
+}
---
+++ workspace-sts-3.9.17.RELEASE/ch2/src/main/java/com/jklee/ch2/Person.java
@@ -0,0 +1,6 @@
+package com.jklee.ch2;
+
+public class Person { 
+	private Car car = new Car(); 
+	public  Car getCar() { return car; }
+}  
